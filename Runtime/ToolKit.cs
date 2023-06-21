d5bbf38a198faf1f1ef01f6dba214048ef03855c
Jphoooo <156303815@qq.com>
2023-06-21 16:16:12 +0800

    Update Editor

--- Runtime/ToolKit.cs
+++ Runtime/ToolKit.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Jphoooo.Tools.Runtimes
 {
-    public static class SampleToolKit
+    public static class ToolKit
     {
         public static string WithColor(this string str, Color col)
         {
