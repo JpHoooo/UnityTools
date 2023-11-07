ccb14c44d9c360f837b2c66ec1f5fe7003b58641
Jphoooo <jphoooooooo@gmail.com>
2023-11-07 14:15:43 +0800

    Optimize the code of Function.cs

--- Runtime/FunctionExtension/Function.cs
+++ Runtime/FunctionExtension/Function.cs
@@ -1,18 +1,51 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 namespace Jphoooo.Tools{
-    public static class Function
+    public class Function
     {
-        public static void Periodic(this MonoBehaviour mono,Action action,float duration){
-            mono.StartCoroutine(Counter(mono, action,duration));
-       }
-        static IEnumerator Counter(this MonoBehaviour mono,Action action, float duration)
-        {
-            action?.Invoke();
-            yield return new WaitForSeconds(duration);      
-            mono.StartCoroutine(Counter(mono,action, duration));
+        private static GameObject monohook = null;
+
+        public static Function Periodic(Action action,float duration){
+            InitMonohook();
+            Function function = new Function(action, duration);
+            // monohook.GetComponent<MonoBehaviourHook>().perioidcAction = action;
+            // monohook.GetComponent<MonoBehaviourHook>().duration = duration;
+            monohook.GetComponent<MonoBehaviourHook>().StartFunction(action, duration);
+            return function;
         }
+
+        private static void InitMonohook(){
+            if(monohook == null){
+                monohook = new GameObject("FunctionMonohook", typeof(MonoBehaviourHook));
+            }
+        }
+
+        Action action;
+        float duration;
+
+        public Function(Action action,float duration){
+            this.action = action;
+            this.duration = duration;
+        }
+
+
+        private class MonoBehaviourHook:MonoBehaviour{
+ 
+            public void StartFunction(Action perioidcAction, float duration){
+                StartCoroutine(Counter(perioidcAction,duration));
+            }
+            
+             IEnumerator Counter(Action action, float duration)
+            {
+                action?.Invoke();
+                yield return new WaitForSeconds(duration);      
+                StartCoroutine(Counter(action, duration));
+            }
+        }
+
     }
 }
 
